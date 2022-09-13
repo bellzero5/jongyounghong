99e3ab8f254a6fb01f68a5c741a9f33e3de53c56
unknown <jongyoung.hong@gmaill.com>
2022-09-13 22:54:17 +0900

    mid

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'jongyoung.hong@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
